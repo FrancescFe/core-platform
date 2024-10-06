7bb589ef85117bcf58f11e8de0eef8b26d670dfc
FrancescFe <fraferru@gmail.com>
2024-10-06 23:29:35 +0200

    ci: added qa-test manual action

--- src/test/java/com/inditex/challenge/coreplatform/prices/qa/PriceQATests.java
+++ src/test/java/com/inditex/challenge/coreplatform/prices/qa/PriceQATests.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PriceQaTests {
+public class PriceQATests {
 
     @Autowired
     private MockMvc mockMvc;
